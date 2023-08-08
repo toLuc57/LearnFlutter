cebd3e5fee68e60139b3419fec9615e362afe275
lucTo57 <nhocphan0@gmail.com>
2023-08-08 12:52:31 +0700

    udpate

---
+++ Theory7/custom_animation/lib/models/ProductItem.dart
@@ -0,0 +1,20 @@
+// ignore: duplicate_ignore
+// ignore: file_names
+// ignore_for_file: file_names
+
+import 'Product.dart';
+
+class ProductItem {
+  int quantity;
+  final Product? product;
+
+  ProductItem({this.quantity = 1, required this.product});
+
+  void increment() {
+    quantity++;
+  }
+
+  // void add() {}
+
+  // void substract() {}
+}
