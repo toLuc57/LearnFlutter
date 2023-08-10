b88eb1fc90fce9fe08f6d60becce2339ef1ee3a1
Le Ha Phan <96456141+toLuc57@users.noreply.github.com>
2023-08-10 11:01:28 +0700

    Delete Session4 directory
    delete

--- Session4/daily_exercises/android/app/src/main/kotlin/com/example/daily_exercises/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.daily_exercises
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Session4/ebook_online_book/android/app/src/main/kotlin/com/example/ebook_online_book/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ebook_online_book
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
