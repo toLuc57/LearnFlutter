c67e6807d6f4bb8837884547e729a977618181c3
lucTo57 <nhocphan0@gmail.com>
2023-08-10 22:50:42 +0700

    update

---
+++ Session4/ebook_online_book/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\AndroidStudio\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\DuLieuSinhVien_DTM\Nam4_HKIII\Mobie\PracticeSession\Session4\ebook_online_book"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
